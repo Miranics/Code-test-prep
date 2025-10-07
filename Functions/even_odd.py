16bbbc82e355403a0dc30b637c99502f21937a41
Miracle Nanen <002nasya@gmail.com>
2025-10-08 00:56:43 +0200

    Add function to count even numbers in a list

---
+++ Functions/even_odd.py
@@ -0,0 +1,10 @@
+def count_even_odd(numbers):
+    count = 0 
+    for num in numbers:
+        if num % 2 == 0:
+            count += 1
+    return count
+        
+print(count_even_odd([1, 2, 3, 4, 5, 6]))       
+        
+
