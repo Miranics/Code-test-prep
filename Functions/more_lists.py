171fa2f598295107b446056413d4423a11c76cac
Miracle Nanen <002nasya@gmail.com>
2025-10-08 03:55:30 +0200

    Add function to return numbers greater than a specified target

---
+++ Functions/more_lists.py
@@ -0,0 +1,10 @@
+def greater_than(numbers, target):
+    result = []
+    for num in numbers:
+
+        if num > single_num:
+            result.append(num)
+    return result
+
+
+print(greater_than([2, 8, 3, 10, 15, 5], 6))
